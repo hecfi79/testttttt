fb8c2f8d45deb99a4709c3c6b47965046e57b36a
hecfi79 <106524689+hecfi79@users.noreply.github.com>
2022-06-08 22:07:58 +0600

    ddd

--- app.py
+++ app.py
@@ -1,48 +1,34 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
+from vega_datasets import data
 
 
 def main():
-    df = pd.read_csv('https://pastebin.com/raw/NaZLBe1N')
-    df.drop(df.columns[0], axis=1, inplace=True)
-    X = np.array(df.drop(['Classes  '], axis=1))
-    y = np.array(df['Classes  '])
-    y = y.astype(np.int64)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-    clf = KNeighborsClassifier()
-    clf.fit(X_train, y_train)
-    page = st.sidebar.selectbox("Выберите страницу", ('Основная', 'Все, связанное с моделью'))
-    if page == 'Основная':
-        st.title('Исследуем датасет с пожарами в Алжире')
-        st.write("Просто датафрейм")
-        st.dataframe(df)
-    elif page == 'Все, связанное с моделью':
-        st.title('Исследуем датасет с пожарами в Алжире')
-        st.title('Наконец заработало!!')
-        st.markdown('### Анализ связей столбцов')
-        st.text('Корреляция:')
-        fig, ax = plt.subplots(figsize=(10, 10))
-        sns.heatmap(df.corr(), annot=True, ax=ax)
-        st.pyplot(fig)
-        st.text('Влияние различных классов')
-        cols = ['Classes  ', 'ISI', 'FWI', ' RH', 'FFMC']
-        sns_plot = sns.pairplot(df[cols])
-        st.pyplot(sns_plot)
-        st.title('Моделируем')
-        st.write("Точность, просто датафрейм, предсказание")
-        st.write("Точность")
-        st.write(str(clf.score(X_test, y_test)))
-        st.write("Просто датафрейм")
-        st.dataframe(df)
-        st.write("Предсказание")
-        df2 = pd.DataFrame({'Реальность': y_test, 'Предсказание': clf.predict(X_test)})
-        st.dataframe(df2)
+    df = load_data()
+    page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
 
+    if page == "Homepage":
+        st.header("This is your data explorer.")
+        st.write("Please select a page on the left.")
+        st.write(df)
+    elif page == "Exploration":
+        st.title("Data Exploration")
+        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
+        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
+        visualize_data(df, x_axis, y_axis)
 
-if __name__ == '__main__':
+@st.cache
+def load_data():
+    df = data.cars()
+    return df
+
+def visualize_data(df, x_axis, y_axis):
+    graph = alt.Chart(df).mark_circle(size=60).encode(
+        x=x_axis,
+        y=y_axis,
+        color='Origin',
+        tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
+    ).interactive()
+
+    st.write(graph)
+
+if __name__ == "__main__":
     main()
